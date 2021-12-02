d44ebd2bd81641201056d532817f70e67cabc165
Asko Nõmm <asko@bien.ee>
2021-12-02 01:31:29 -0300

    Implement heading block

--- src/clarktown/parsers/heading_block.clj
+++ src/clarktown/parsers/heading_block.clj
@@ -2,12 +2,26 @@
   (:require
     [clojure.string :as string]))
 
+
 (defn is?
+  "Determines whether the given block is a heading block or not."
   [block]
-  (= true (-> (clojure.string/trim block)
+  (= true (-> (string/replace block #"\n" "")
+              string/trim
               (string/starts-with? "#"))))
 
 
 (defn render
+  "Renders the heading block."
   [block]
-  "headingblock")
+  (let [single-line-block (-> (string/replace block #"\n" "")
+                              string/trim)
+        size-indicators (-> (string/split single-line-block #" ")
+                            first
+                            string/trim)
+        size (count size-indicators)
+        value (->> (string/split single-line-block #" ")
+                   next
+                   (string/join " ")
+                   string/trim)]
+    (str "<h" size ">" value "</h" size ">")))
