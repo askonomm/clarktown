8a5e868098005401ff7cba45ec50e9c6612550f4
Asko Nõmm <asko@bien.ee>
2021-12-04 17:33:28 -0300

    Add italic text renderer

---
+++ src/clarktown/parsers/italic.clj
@@ -0,0 +1,18 @@
+(ns clarktown.parsers.italic
+  (:require
+    [clojure.string :as string]))
+
+
+(defn render
+  "Renders all occuring italic text as italic."
+  [block]
+  (loop [block block
+         matches (-> (re-seq #"_.*?_" block)
+                     distinct)]
+    (if (empty? matches)
+      block
+      (let [match (first matches)
+            value (subs match 1 (- (count match) 1))
+            replacement (str "<em>" value "</em>")]
+        (recur (string/replace block match replacement)
+               (drop 1 matches))))))
