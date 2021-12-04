17f3b8c45ab395b5fe03d2358defe01f38ee727d
Asko Nõmm <asko@bien.ee>
2021-12-04 17:34:04 -0300

    Add link and image renderer

---
+++ src/clarktown/parsers/link_and_image.clj
@@ -0,0 +1,21 @@
+(ns clarktown.parsers.link-and-image
+  (:require
+    [clojure.string :as string]))
+
+
+(defn render
+  "Renders all occuring links and images."
+  [block]
+  (loop [block block
+         matches (-> (re-seq #"\!?\[(.*?)\]\((.*?)\)" block)
+                     distinct)]
+    (if (empty? matches)
+      block
+      (let [[whole-match label href] (first matches)
+            image? (string/starts-with? whole-match "!")
+            image (str "<img src=\"" href "\" alt=\"" label "\">")
+            link (str "<a href=\"" href "\">" label "</a>")]
+        (recur (if image?
+                 (string/replace block whole-match image)
+                 (string/replace block whole-match link))
+               (drop 1 matches))))))
