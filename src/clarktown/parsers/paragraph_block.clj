d2b88d831fe7402743cccec9e50ca00e6112f614
Asko Nõmm <asko@bien.ee>
2021-12-07 22:21:32 -0300

    Add paragraph block

---
+++ src/clarktown/parsers/paragraph_block.clj
@@ -0,0 +1,9 @@
+(ns clarktown.parsers.paragraph-block
+  (:require
+    [clojure.string :as string]))
+
+
+(defn render
+  "Renders the paragraph block."
+  [block _]
+  (str "<p>" (string/trim block) "</p>"))
