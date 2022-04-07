c0c8a2e49d395b574c7a31727c06f315139d7336
Asko Nomm <asko@bien.ee>
2022-04-07 11:08:32 +0200

    Add tests for italic text, and support using `*` for italic text creation as well (re #5)

--- src/clarktown/parsers/italic.clj
+++ src/clarktown/parsers/italic.clj
@@ -7,11 +7,11 @@
   "Renders all occurring italic text as italic."
   [block _]
   (loop [block block
-         matches (-> (re-seq #"_.*?_" block)
+         matches (-> (re-seq #"(\*{1}|\_{1})(.*?)(\*{1}|\_{1})" block)
                      distinct)]
     (if (empty? matches)
       block
-      (let [match (first matches)
+      (let [match (ffirst matches)
             value (subs match 1 (- (count match) 1))
             replacement (str "<em>" value "</em>")]
         (recur (string/replace block match replacement)
