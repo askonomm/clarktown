842d64c8f02a7958e4201660b05f2c17cdd9b6dc
Asko Nomm <asko@bien.ee>
2022-04-07 11:05:25 +0200

    Add tests for bold text, and support using `__` for bold text creation as well (re #5)

--- src/clarktown/parsers/bold.clj
+++ src/clarktown/parsers/bold.clj
@@ -7,11 +7,11 @@
   "Renders all occurring bold text as bold."
   [block _]
   (loop [block block
-         matches (-> (re-seq #"\*\*.*?\*\*" block)
+         matches (-> (re-seq #"(\*{2}|\_{2})(.*?)(\*{2}|\_{2})" block)
                      distinct)]
     (if (empty? matches)
       block
-      (let [match (first matches)
+      (let [match (ffirst matches)
             value (subs match 2 (- (count match) 2))
             replacement (str "<strong>" value "</strong>")]
         (recur (string/replace block match replacement)
