9fae4b6dfabc7485a8a2debf108d8190761590f5
Asko Nomm <asko@bien.ee>
2022-04-09 19:32:05 +0200

    Fix #8: Links break textual checkboxes

--- src/clarktown/parsers/link_and_image.clj
+++ src/clarktown/parsers/link_and_image.clj
@@ -7,11 +7,11 @@
   "Renders all occurring links and images."
   [block _]
   (loop [block block
-         matches (-> (re-seq #"\!?\[(.*?)\]\((.*?)\)" block)
+         matches (-> (re-seq #"\!?\[(\w+( \w+|\.|\,)*)\]\((.*?)\)" block)
                      distinct)]
     (if (empty? matches)
       block
-      (let [[whole-match label href] (first matches)
+      (let [[whole-match label _ href] (first matches)
             image? (string/starts-with? whole-match "!")
             image (str "<img src=\"" href "\" alt=\"" label "\">")
             link (str "<a href=\"" href "\">" label "</a>")]
