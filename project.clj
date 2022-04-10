8aa7f6aac2ca359b810bdc4f7a5ad143c6642a06
Asko Nomm <asko@bien.ee>
2022-04-10 20:51:27 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.github.askonomm/clarktown "1.0.3"
+(defproject com.github.askonomm/clarktown "1.1.0"
   :description "A zero-dependency, pure-clojure Markdown parser."
   :url "https://github.com/askonomm/clarktown"
   :license {:name "MIT"
             :url "https://github.com/askonomm/clarktown/blob/master/LICENSE.txt"}
   :deploy-repositories [["releases"  {:sign-releases false :url "https://repo.clojars.org"}]
                         ["snapshots" {:sign-releases false :url "https://repo.clojars.org"}]]
-  :dependencies [[org.clojure/clojure "1.11.0"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :plugins [[lein-auto "0.1.3"]
             [lein-cloverage "1.2.3"]]
   :repl-options {:init-ns clarktown.core})
