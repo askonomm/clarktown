dc921cbe1c786995a8670efd9f5556ee9df064f9
Asko Nõmm <asko@bien.ee>
2022-04-23 02:42:52 +0300

    Add docstrings

--- src/clarktown/correctors/atx_heading_block.clj
+++ src/clarktown/correctors/atx_heading_block.clj
@@ -4,6 +4,11 @@
 
 
 (defn empty-line-above?
+  "Determines whether there's a need for an empty new line 
+  above the `line` at the current `index`. In the case of a 
+  ATX heading block that starts with the `#` character, if 
+  there's no empty newline above, we need to create one, and 
+  so this function must then return `true`."
   [lines line index]
   (and (string/starts-with? (string/trim line) "#")
        (> index 0)
@@ -12,6 +17,11 @@
 
 
 (defn empty-line-below?
+  "Determines whether there's a need for an empty new line
+  below the `line` at the current `index`. In the case of a 
+  ATX heading block that starts with the `#` character, if
+  there's no empty newline below, we need to create one, and
+  so this function must then return `true`."
   [lines line index]
   (and (string/starts-with? (string/trim line) "#")
        (< index (- (count lines) 1))
--- src/clarktown/correctors/code_block.clj
+++ src/clarktown/correctors/code_block.clj
@@ -4,6 +4,11 @@
 
 
 (defn empty-line-above?
+  "Determines whether there's a need for an empty new line
+  above the `line` at the current `index`. In the case of a 
+  code block, which starts with three backticks (```), if there's
+  no empty newline above, we need to create one, and so this
+  function must then return `true`."
   [lines line index]
   (and (string/starts-with? (string/trim line) "```")
        (> index 0)
@@ -16,6 +21,11 @@
 
 
 (defn empty-line-below?
+  "Determines whether there's a need for an empty new line
+  below the `line` at the current `index`. In the case of a 
+  code block, which ends with three backticks (```), if there's
+  no empty newline above, we need to create one, and so this 
+  function must then return `true`."
   [lines line index]
   (and (string/starts-with? (string/trim line) "```")
        (< index (- (count lines) 1))
