829a5b1d96f2fd04f992dd3792d0faac5aacc2a8
Asko Nõmm <asko@bien.ee>
2022-04-25 12:21:39 +0300

    Bump dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.askonomm/clarktown "1.1.2"
+(defproject com.github.askonomm/clarktown "2.0.0-alpha8"
   :description "A zero-dependency, pure-clojure Markdown parser."
   :url "https://github.com/askonomm/clarktown"
   :license {:name "MIT"
