dc2a364db6d14d4afa9da9aadbccb8fdbb038fe4
Asko Nõmm <ano@ano.ee>
2022-05-12 21:07:14 +0300

    Add tests

--- test/clarktown/correctors/indented_code_block_test.clj
+++ test/clarktown/correctors/indented_code_block_test.clj
@@ -1,30 +1,30 @@
-(ns clarktown.correctors.atx-heading-block-test
-  (:require 
+(ns clarktown.correctors.indented-code-block-test
+  (:require
     [clojure.test :refer [deftest testing is]]
-    [clarktown.correctors.atx-heading-block :as corrector]))
+    [clarktown.correctors.indented-code-block :as corrector]))
 
 
-(deftest atx-heading-block-corrector-test
+(deftest indented-code-block-corrector-test
   (testing "Empty line above"
-    (let [line "# Hello"
+    (let [line "    Test"
           lines ["Some text goes here" line]
           index 1]
       (is (true? (corrector/newline-above? lines line index)))))
-    
+
   (testing "No empty line above"
-    (let [line "# Hello" 
+    (let [line "    Hello"
           lines ["Some text goes here" "\n" line]
           index 2]
       (is (false? (corrector/newline-above? lines line index)))))
-    
+
   (testing "Empty line below"
-    (let [line "# Hello"
+    (let [line "    Hello"
           lines [line "Some text goes here"]
           index 0]
       (is (true? (corrector/newline-below? lines line index)))))
-    
+
   (testing "No empty line below"
-    (let [line "# Hello" 
+    (let [line "    Hello"
           lines [line "\n" "Some text goes here"]
           index 0]
       (is (false? (corrector/newline-below? lines line index))))))
